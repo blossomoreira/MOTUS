48186c627c556fe918193c28243ca41ba3a3bbe4
Diogo Moreira <104456651+diogomoreirax@users.noreply.github.com>
2024-04-16 22:15:15 -0300

    Storymaker page

--- motus/pages/storymaker.js
+++ motus/pages/storymaker.js
@@ -1,18 +1,12 @@
-
 import Link from 'next/link';
-
 
 export default function Storymaker() {
   return (
-  
     <div className="container">
-
       <nav className="navbar">
-
         <div className="logo-container">
           <img src="/midia/vmv.png" alt="Logo MOTUS" width="100" height="100" />
         </div>
-
         <ul className="nav-links">
           <li className="nav-item">
             <Link href="/sobre-nos">Sobre Nós</Link>
@@ -29,14 +23,54 @@ export default function Storymaker() {
         </ul>
       </nav>
 
-
       <div className="textdiv">
-        <p>Oferecemos serviços de Story Maker para eventos, proporcionando uma experiência única e envolvente. Capturamos momentos especiais em tempo real e os transformamos em conteúdos irresistíveis.
-
-Garantimos que cada história seja uma obra-prima, editada e publicada com qualidade excepcional. Fornecemos vídeos curtos de DJs, performances e interações do público, selecionados para destacar a energia e a emoção da ocasião.
-
-Destacamos a atração principal do evento, criando expectativa e interesse. Compartilhamos histórias autênticas do público, conectando sua marca de forma significativa, refletindo positivamente na percepção da sua marca.</p>
+        <p>Oferecemos serviços de Story Maker para eventos, proporcionando uma experiência única e envolvente. Capturamos momentos especiais em tempo real e os transformamos em conteúdos irresistíveis. Garantimos que cada história seja uma obra-prima, editada e publicada com qualidade excepcional. Fornecemos vídeos curtos de DJs, performances e interações do público, selecionados para destacar a energia e a emoção da ocasião. Destacamos a atração principal do evento, criando expectativa e interesse. Compartilhamos histórias autênticas do público, conectando sua marca de forma significativa, refletindo positivamente na percepção da sua marca.</p>
       </div>
+
+
+
+      <div className="videos-container">
+
+
+
+        <div className="video-wrapper">
+          <video className="video" controls>
+            <source src="/midia/Bloblosom/Depoimento-Matheus.mp4" type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
+          <div className="video-caption">
+            Texto de legenda ou descrição do vídeo aqui.
+          </div>
+        </div>
+
+
+        <div className="video-wrapper">
+          <video className="video" controls>
+            <source src="/midia/Bloblosom/Depoimento-Matheus.mp4" type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
+          <div className="video-caption">
+            Texto de legenda ou descrição do vídeo aqui.
+          </div>
+        </div>
+
+
+        <div className="video-wrapper">
+          <video className="video" controls>
+            <source src="/midia/Bloblosom/Depoimento-Matheus.mp4" type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
+          <div className="video-caption">
+            Texto de legenda ou descrição do vídeo aqui.
+          </div>
+        </div>
+
+
+      </div>
+
+
+
+
 
     </div>
   );
