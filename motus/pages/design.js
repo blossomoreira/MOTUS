236615030644751b4277940c22d9146dda561bb4
Diogo Moreira <104456651+diogomoreirax@users.noreply.github.com>
2024-07-12 00:40:46 -0300

    oii

--- motus/pages/design.js
+++ motus/pages/design.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Reels() {
     return (
       <div>
-        <h1>About Page</h1>
+        <h1> </h1>
         <p>This is the About page.</p>
       </div>
     );
